d2f184f73ebb36ab6984b916301712fc1fbd4c3f
Dionid <dionid@Dionids-MacBook-Air.local>
2022-03-28 01:32:57 +0300

    feat: added z to spawning heroes

--- frontend/src/libs/bgs/ecs/systems/spawn-hero-system/index.ts
+++ frontend/src/libs/bgs/ecs/systems/spawn-hero-system/index.ts
@@ -20,66 +20,71 @@ export const SpawnHeroSystem = (): System<{
   SizeComponent: SizeComponent;
   DraggableComponent: DraggableComponent;
   SelectableComponent: SelectableComponent;
-}> => ({
-  run: async (world) => {
-    const entities = World.filter(world, ['SpawnHeroComponent']);
-    if (entities.length === 0) {
-      return;
-    }
+}> => {
+  let lastZ = 1;
 
-    const spawnHeroComponentPool = World.getOrAddPool(world, 'SpawnHeroComponent');
-    const reactHeroComponentPool = World.getOrAddPool(world, 'ReactHeroComponent');
-    const imageComponentPool = World.getOrAddPool(world, 'ImageComponent');
-    const positionComponentPool = World.getOrAddPool(world, 'PositionComponent');
-    const sizeComponentPool = World.getOrAddPool(world, 'SizeComponent');
-    const draggableComponentPool = World.getOrAddPool(world, 'DraggableComponent');
-    const selectableComponentPool = World.getOrAddPool(world, 'SelectableComponent');
+  return {
+    run: async (world) => {
+      const entities = World.filter(world, ['SpawnHeroComponent']);
+      if (entities.length === 0) {
+        return;
+      }
 
-    for (const entity of entities) {
-      const spawnComponent = Pool.get(spawnHeroComponentPool, entity);
-      const heroEntity = EntityId.new();
-      // . Create react game map
-      Pool.add(reactHeroComponentPool, heroEntity, {
-        id: ComponentId.new(),
-        name: 'ReactHeroComponent',
-        data: {},
-      });
-      Pool.add(imageComponentPool, heroEntity, {
-        id: ComponentId.new(),
-        name: 'ImageComponent',
-        data: {
-          url: spawnComponent.data.url,
-        },
-      });
-      Pool.add(positionComponentPool, heroEntity, {
-        id: ComponentId.new(),
-        name: 'PositionComponent',
-        data: {
-          x: 100,
-          y: 100,
-          z: 1,
-        },
-      });
-      Pool.add(sizeComponentPool, heroEntity, {
-        id: ComponentId.new(),
-        name: 'SizeComponent',
-        data: {
-          width: 60,
-          height: 100,
-        },
-      });
-      Pool.add(draggableComponentPool, heroEntity, {
-        id: ComponentId.new(),
-        name: 'DraggableComponent',
-        data: {},
-      });
-      Pool.add(selectableComponentPool, heroEntity, {
-        id: ComponentId.new(),
-        name: 'SelectableComponent',
-        data: {},
-      });
-      // . Destroy component
-      Pool.delete(spawnHeroComponentPool, entity);
-    }
-  },
-});
+      const spawnHeroComponentPool = World.getOrAddPool(world, 'SpawnHeroComponent');
+      const reactHeroComponentPool = World.getOrAddPool(world, 'ReactHeroComponent');
+      const imageComponentPool = World.getOrAddPool(world, 'ImageComponent');
+      const positionComponentPool = World.getOrAddPool(world, 'PositionComponent');
+      const sizeComponentPool = World.getOrAddPool(world, 'SizeComponent');
+      const draggableComponentPool = World.getOrAddPool(world, 'DraggableComponent');
+      const selectableComponentPool = World.getOrAddPool(world, 'SelectableComponent');
+
+      for (const entity of entities) {
+        const spawnComponent = Pool.get(spawnHeroComponentPool, entity);
+        const heroEntity = EntityId.new();
+        // . Create react game map
+        Pool.add(reactHeroComponentPool, heroEntity, {
+          id: ComponentId.new(),
+          name: 'ReactHeroComponent',
+          data: {},
+        });
+        Pool.add(imageComponentPool, heroEntity, {
+          id: ComponentId.new(),
+          name: 'ImageComponent',
+          data: {
+            url: spawnComponent.data.url,
+          },
+        });
+        Pool.add(positionComponentPool, heroEntity, {
+          id: ComponentId.new(),
+          name: 'PositionComponent',
+          data: {
+            x: 100,
+            y: 100,
+            z: lastZ + 1,
+          },
+        });
+        Pool.add(sizeComponentPool, heroEntity, {
+          id: ComponentId.new(),
+          name: 'SizeComponent',
+          data: {
+            width: 60,
+            height: 100,
+          },
+        });
+        Pool.add(draggableComponentPool, heroEntity, {
+          id: ComponentId.new(),
+          name: 'DraggableComponent',
+          data: {},
+        });
+        Pool.add(selectableComponentPool, heroEntity, {
+          id: ComponentId.new(),
+          name: 'SelectableComponent',
+          data: {},
+        });
+        // . Destroy component
+        Pool.delete(spawnHeroComponentPool, entity);
+        lastZ += 1;
+      }
+    },
+  };
+};
