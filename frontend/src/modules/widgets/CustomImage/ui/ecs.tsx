0b94627c0e5fa7f9129ae6ac08d782b3c42ac5d6
Dionid <dionid@Dionids-MacBook-Air.local>
2022-03-29 02:05:14 +0300

    feat: added rounds to corners

--- frontend/src/modules/widgets/CustomImage/ui/ecs.tsx
+++ frontend/src/modules/widgets/CustomImage/ui/ecs.tsx
@@ -3,6 +3,21 @@ import { BgsIgnitor } from '../../../../libs/bgs/ecs';
 import { useEcsComponent } from '../../../../libs/ecs/react';
 import { CustomImage } from './index';
 import React from 'react';
+import { Group } from 'react-konva';
+
+function calcClipFunc(ctx: any, x: number, y: number, width: number, height: number, radius: number) {
+  ctx.beginPath();
+  ctx.moveTo(x + radius, y);
+  ctx.lineTo(x + width - radius, y);
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+  ctx.lineTo(x + width, y + height - radius);
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+  ctx.lineTo(x + radius, y + height);
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+  ctx.lineTo(x, y + radius);
+  ctx.quadraticCurveTo(x, y, x + radius, y);
+  ctx.closePath();
+}
 
 export const ECSCustomImage = React.memo((props: { entity: EntityId; ignitor: BgsIgnitor }) => {
   const { entity, ignitor } = props;
@@ -12,15 +27,19 @@ export const ECSCustomImage = React.memo((props: { entity: EntityId; ignitor: Bg
   const size = useEcsComponent(entity, { width: 0, height: 0 }, 'ReactSizeComponent', ignitor);
 
   return (
-    <CustomImage
-      key={entity}
-      isSelected={false}
-      onSelect={() => {}}
-      url={image.url}
-      width={size.width}
-      height={size.height}
-      x={position.x}
-      y={position.y}
-    />
+    <Group clipFunc={(ctx: any) => calcClipFunc(ctx, position.x, position.y, size.width, size.height, 3)}>
+      <CustomImage
+        key={entity}
+        isSelected={false}
+        onSelect={() => {}}
+        url={image.url}
+        width={size.width}
+        height={size.height}
+        x={position.x}
+        y={position.y}
+        stroke={'#fff'}
+        strokeWidth={3}
+      />
+    </Group>
   );
 });
