cb5ee9cb80dc8025d71e3cfd425f760bcde2aa9c
David Shek <ditreyw@gmail.com>
2024-07-17 12:03:22 +0800

    feat: added primitives

--- new-front/src/libs/tecs-tecs/schema.ts
+++ new-front/src/libs/tecs-tecs/schema.ts
@@ -4,27 +4,89 @@ import { Id } from './core';
 export const kind = Symbol('kind');
 export const defaultFn = Symbol('defaultFn');
 
-export const numberK = Symbol('number');
-export const float64K = Symbol('float64');
-export const stringK = Symbol('string');
+// # Kinds
 
-export const number = {
-  [kind]: numberK,
+export const uint8K = Symbol('uint8');
+export const uint16K = Symbol('uint16');
+export const uint32K = Symbol('uint32');
+
+export const int8K = Symbol('int8');
+export const int16K = Symbol('int16');
+export const int32K = Symbol('int32');
+
+export const float32K = Symbol('float32');
+export const float64K = Symbol('float64');
+
+export const string8K = Symbol('string8');
+export const string16K = Symbol('string16');
+
+export const booleanK = Symbol('boolean');
+
+export const numberK = float64K;
+export const stringK = string16K;
+
+export const uint8 = {
+  [kind]: uint8K,
+  byteLength: 1,
+  [defaultFn]: () => 0,
+} as const;
+
+export const uint16 = {
+  [kind]: uint16K,
   byteLength: 2,
+  [defaultFn]: () => 0,
+} as const;
+
+export const uint32 = {
+  [kind]: uint32K,
+  byteLength: 4,
+  [defaultFn]: () => 0,
+} as const;
+
+export const int8 = {
+  [kind]: int8K,
+  byteLength: 1,
+  [defaultFn]: () => 0,
+} as const;
+
+export const int16 = {
+  [kind]: int16K,
+  byteLength: 2,
+  [defaultFn]: () => 0,
+} as const;
+
+export const int32 = {
+  [kind]: int32K,
+  byteLength: 4,
+  [defaultFn]: () => 0,
+} as const;
+
+export const float32 = {
+  [kind]: float32K,
+  byteLength: 4,
   [defaultFn]: () => 0,
 } as const;
 
 export const float64 = {
   [kind]: float64K,
-  byteLength: 2,
+  byteLength: 8,
   [defaultFn]: () => 0,
 } as const;
 
-export const string = {
-  [kind]: stringK,
+export const string8 = {
+  [kind]: string8K,
+  byteLength: 1,
+  [defaultFn]: () => '',
+};
+
+export const string16 = {
+  [kind]: string16K,
   byteLength: 2,
   [defaultFn]: () => '',
 };
+
+export const number = float64;
+export const string = string16;
 
 export type Types = typeof number | typeof float64 | typeof string;
 
