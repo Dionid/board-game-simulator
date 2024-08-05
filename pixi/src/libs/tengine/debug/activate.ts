765797f0d9a8aef6bee283ed53f0f240fa86bbcf
David Shek <ditreyw@gmail.com>
2024-08-05 15:04:58 +0800

    feat: added meta to events

--- pixi/src/libs/tengine/debug/activate.ts
+++ pixi/src/libs/tengine/debug/activate.ts
@@ -1,14 +1,8 @@
-import { registerSystem, registerTopic } from 'libs/tecs';
+import { registerSystem } from 'libs/tecs';
 import { Game } from '../game';
 import { DEBUG } from './core';
 import { drawDebug } from './render';
-import {
-  componentUpdated,
-  entityKilled,
-  entitySpawned,
-  schemaAdded,
-  schemaRemoved,
-} from 'libs/tecs/default-topics';
+import { debugEvents } from './events';
 
 export function activateDebugMode(
   game: Game,
@@ -31,71 +25,13 @@ export function activateDebugMode(
 ) {
   DEBUG.isActive = true;
 
-  const events = options.events ?? {
-    entitySpawned: true,
-    entityKilled: true,
-    schemaAdded: true,
-    schemaRemoved: true,
-    componentUpdated: true,
-  };
-
-  if (events.entitySpawned) {
-    registerTopic(game.essence, entitySpawned);
-  }
-  if (events.entityKilled) {
-    registerTopic(game.essence, entityKilled);
-  }
-  if (events.schemaAdded) {
-    registerTopic(game.essence, schemaAdded);
-  }
-  if (events.schemaRemoved) {
-    registerTopic(game.essence, schemaRemoved);
-  }
-  if (events.componentUpdated) {
-    registerTopic(game.essence, componentUpdated);
-  }
-
   // # Render debug lines
   registerSystem(game.essence, drawDebug(game, options.render || {}), {
     stage: 'postUpdate',
   });
 
   // # Debug default events
-  registerSystem(
-    game.essence,
-    () => {
-      if (events.entitySpawned) {
-        for (const event of entitySpawned) {
-          console.log('Entity spawned:', event);
-        }
-      }
-
-      if (events.entityKilled) {
-        for (const event of entityKilled) {
-          console.log('Entity killed:', event);
-        }
-      }
-
-      if (events.schemaAdded) {
-        for (const event of schemaAdded) {
-          console.log('Schema added:', event);
-        }
-      }
-
-      if (events.schemaRemoved) {
-        for (const event of schemaRemoved) {
-          console.log('Schema removed:', event);
-        }
-      }
-
-      if (events.componentUpdated) {
-        for (const event of componentUpdated) {
-          console.log('Component updated:', event);
-        }
-      }
-    },
-    {
-      stage: 'postUpdate',
-    }
-  );
+  registerSystem(game.essence, debugEvents(game, options.events), {
+    stage: 'postUpdate',
+  });
 }
---
+++ pixi/src/libs/tengine/debug/events.ts
@@ -0,0 +1,83 @@
+import { registerTopic, TopicEvent, topicEventMeta } from 'libs/tecs';
+import {
+  entitySpawned,
+  entityKilled,
+  schemaAdded,
+  schemaRemoved,
+  componentUpdated,
+} from 'libs/tecs/default-topics';
+import { Game } from '../game';
+
+export function debugEvents(
+  game: Game,
+  opts: {
+    entitySpawned?: boolean;
+    entityKilled?: boolean;
+    schemaAdded?: boolean;
+    schemaRemoved?: boolean;
+    componentUpdated?: boolean;
+  } = {
+    entitySpawned: true,
+    entityKilled: true,
+    schemaAdded: true,
+    schemaRemoved: true,
+    componentUpdated: true,
+  }
+) {
+  if (opts.entitySpawned) {
+    registerTopic(game.essence, entitySpawned);
+  }
+  if (opts.entityKilled) {
+    registerTopic(game.essence, entityKilled);
+  }
+  if (opts.schemaAdded) {
+    registerTopic(game.essence, schemaAdded);
+  }
+  if (opts.schemaRemoved) {
+    registerTopic(game.essence, schemaRemoved);
+  }
+  if (opts.componentUpdated) {
+    registerTopic(game.essence, componentUpdated);
+  }
+  return () => {
+    const events: TopicEvent<Record<PropertyKey, any>>[] = [];
+
+    if (opts.entitySpawned) {
+      for (const event of entitySpawned) {
+        events.push(event);
+      }
+    }
+
+    if (opts.entityKilled) {
+      for (const event of entityKilled) {
+        events.push(event);
+      }
+    }
+
+    if (opts.schemaAdded) {
+      for (const event of schemaAdded) {
+        events.push(event);
+      }
+    }
+
+    if (opts.schemaRemoved) {
+      for (const event of schemaRemoved) {
+        events.push(event);
+      }
+    }
+
+    if (opts.componentUpdated) {
+      for (const event of componentUpdated) {
+        events.push(event);
+      }
+    }
+
+    events.sort((a, b) => {
+      return a[topicEventMeta].createdAt - b[topicEventMeta].createdAt;
+    });
+
+    for (const event of events) {
+      console.log(`${event.name ?? ''}`, event);
+    }
+  };
+}
